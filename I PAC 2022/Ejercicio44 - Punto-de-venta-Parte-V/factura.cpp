57d8011a1572ee46d4685b538dfec36f411a9541
Delvyn David Estrada <41923892+elchele504@users.noreply.github.com>
2022-04-10 16:51:12 -0600

    ACTUALIZACION TAREA 6

--- I PAC 2022/Ejercicio44 - Punto-de-venta-Parte-V/factura.cpp
+++ I PAC 2022/Ejercicio44 - Punto-de-venta-Parte-V/factura.cpp
@@ -2,29 +2,39 @@
 
 using namespace std;
 
-double subtotal;
+double subtotal, total;
 string listaProductos;
 
-
-void agregarProducto (string descripcion, int cantidad, double precio)
+void agregarProducto(string descripcion,int cantidad, double precio)
 {
     listaProductos = listaProductos + descripcion + '\n';
     subtotal = subtotal + (cantidad * precio);
+    total = (subtotal * 0.15 + subtotal);
 }
 
-void imprimirFactura ()
+void imprimirFactura()
 {
-    system ("cls");
-    cout << "**********"  << endl; 
-    cout << " FACTURA  "  << endl; 
-    cout << "**********"  << endl;
+    system("cls");
+    cout << "********" << endl;
+    cout << "FACTURA" << endl;
+    cout << "********" << endl;
     cout << endl;
 
     cout << "Productos: " << endl;
-    cout <<  listaProductos;
+    cout << listaProductos;
 
     cout << endl;
     cout << "Subtotal: " << subtotal;
     cout << endl;
-    system ("pause");
+    cout << endl;
+
+    cout << endl;
+    cout << "Impuesto: " << "15%";
+    cout << endl;
+
+    cout << endl;
+    cout << "Total: " << total;
+    cout << endl;
+
+    system("pause");
 }
--- I PAC 2022/Ejercicio44 - Punto-de-venta-Parte-V/productos.cpp
+++ I PAC 2022/Ejercicio44 - Punto-de-venta-Parte-V/productos.cpp
@@ -19,6 +19,8 @@ void productos (int opcion)
         cout << "*****************" << endl;
         cout << "1 - Capuccino" << endl;
         cout << "2 - Expresso" << endl;
+        cout << "3 - Macchiato de caramelo" << endl;
+        cout << "4 - Cafe Moca" << endl;
         cout << endl;
 
         cout << "Ingrese una opcion: ";
@@ -32,6 +34,14 @@ void productos (int opcion)
                         
         case 2:
             agregarProducto ("1 Expresso -  L 30.00", 1, 30);
+            break;
+                        
+        case 3:
+            agregarProducto ("1 Macchiato de caramelo -  L 35.00", 1, 35);
+            break;
+                        
+        case 4:
+            agregarProducto ("1 Cafe Moca -  L 25.00", 1, 25);
             break;
                         
         default:
@@ -53,6 +63,44 @@ void productos (int opcion)
     {
         cout << "BEBIDAS FRIAS" << endl;
         cout << "*************" << endl;
+        cout << "1 - White Frappe" << endl;
+        cout << "2 - Granita" << endl;
+        cout << "3 - Frozen Tea" << endl;
+        cout << "4 - Malteada Gelato" << endl;
+        cout << endl;
+        
+
+        cout << "Ingrese una opcion: ";
+        cin >> opcionProducto;
+
+        switch (opcionProducto)
+        {
+        case 1:
+            agregarProducto ("1 White Frappe - L 60.00", 1, 60);
+            break;
+                        
+        case 2:
+            agregarProducto ("1 Granita -  L 60.00", 1, 60);
+            break;
+                        
+        case 3:
+            agregarProducto ("1 Frozen tea -  L 55.00", 1, 55);
+            break;
+                        
+        case 4:
+            agregarProducto ("1 Malteada Gelato -  L 89.00", 1, 89);
+            break;
+                        
+        default:
+            {
+                cout << "opcion no valida";
+                return;
+                break;
+            }
+        }
+
+        cout << endl;
+        cout << "Producto agregado" << endl << endl;
         system ("pause");
         break;
     }
@@ -60,6 +108,44 @@ void productos (int opcion)
     {
         cout << "REPOSTERIA" << endl;
         cout << "**********" << endl;
+        cout << "1 - Dona" << endl;
+        cout << "2 - Pastel Tres leches" << endl;
+        cout << "3 - Pastel chocolate" << endl;
+        cout << "4 - Brownis" << endl;
+        cout << endl;
+
+        cout << "Ingrese una opcion: ";
+        cin >> opcionProducto;
+                    
+        switch (opcionProducto)
+        {
+        case 1:
+            agregarProducto ("1 Dona - L 25.00", 1, 25);
+            break;
+                        
+        case 2:
+            agregarProducto ("1 Pastel Tres leches -  L 70.00", 1, 70);
+            break;
+        
+        case 3:
+            agregarProducto ("1 Pastel chocolate - L 75.00", 1, 75);
+            break;
+        
+        case 4:
+            agregarProducto ("1 Brownis - L 25.00", 1, 25);
+            break;
+         
+                        
+        default:
+            {
+                cout << "opcion no valida";
+                return;
+                break;
+            }
+        }
+
+        cout << endl;
+        cout << "Producto agregado" << endl << endl;
         system ("pause");
         break;
     }
